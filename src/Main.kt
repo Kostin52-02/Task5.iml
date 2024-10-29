c49ead686c410a55069b78de3aeddb80ca049df5
Kostin52-02 <157310420+Kostin52-02@users.noreply.github.com>
2024-10-29 11:21:13 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    println("enter an integer n:")
+    val nInput = readlnOrNull() ?: return
+    println("enter the degree base x:")
+    val xInput = readlnOrNull() ?: return
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    try {
+        val n = nInput.toInt()
+        val x = xInput.toInt()
+
+        var y = 0
+        var power = 1
+
+        while (power < n) {
+            power *= x
+            y++
+        }
+
+        if (power == n) {
+            println("integer exponent y = $y")
+        } else {
+            println("the integer exponent does not exist.")
+        }
+    } catch (e: NumberFormatException) {
+        println("please enter a valid integer.")
     }
 }
